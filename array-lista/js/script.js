59e4c5d471fd2c74a238f655ff181e26e023bd62
zGiusee <giusebelforte@gmail.com>
2023-11-24 14:18:28 +0100

    [UPDATE] exercise "array-lista" done

---
+++ array-lista/js/script.js
@@ -0,0 +1,34 @@
+// ESERCIZIO TUTTI I FRUTTI
+
+// CREO L'ARRAY CHE CONTIENE I FRUTTI
+let frigo = ["mela","pera","ciliegia","arancia","mandarino","cocomero","limone","fragola"];
+console.log(frigo);
+
+// METTO LA PESCA CHE è SUL "TAVOLO" NEL "FRIGO"
+
+let pesca = "pesca";
+frigo.push(pesca)
+
+// CREO LA FLAG
+let check = false;
+
+// CREO L'ALGORIMTO PER CERCARE IL COCOMERO
+for(let i = 0; i<frigo.length; i++){
+
+    if(frigo[i] === "cocomero"){
+        check = true;
+    }
+
+}
+
+// CONTROLLO SE IL COCOMERO è PRESENTE O NO E STAMPO IL MESSAGIO
+
+if(check){
+    console.log("Trovato! Devo solo preparare il cocktail.")
+}
+else{
+    console.log("Oh no, devo uscire a comprare il cocomero!")
+}
+
+// VERIFICO SE è STATO EFFETTUATO TUTTO CORRETTAMENTE
+console.log(frigo);
